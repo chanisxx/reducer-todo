ab3e29a94d17bbe3000dee2101484b212988a232
Chanis Torres <wonderland@Chaniss-MacBook-Pro.local>
2020-07-20 23:22:59 -0400

    created app and reducer

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
+
+export const reducer = (state, action) => {
+    switch(action.type) {
+        default: return state;
+    }
+}
+
+export default reducer;
